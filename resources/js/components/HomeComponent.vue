00958e7eb9cba4d6f33e64e32545543b220c9fb3
David Quong <dquong@sfu.ca>
2018-10-12 01:57:20 -0700

    Create and use HomeComponent
    Create VueJS component for homepage, and refactored styling. Currently
    create room button is non-functional.

---
+++ resources/js/components/HomeComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card">
+                  <div id="page-header" class="card-header">Dashboard</div>
+
+                  <div class="card-body">
+                      <div class="row justify-content-center mb-2">
+                         <div class="col-12 col-sm-6">
+                              <button type="submit" class="home-btn btn btn-primary btn-block">
+                                  Create Room
+                              </button>
+                          </div>
+                      </div>
+                      <div class="row justify-content-center">
+                         <div class="col-12 col-sm-6">
+                              <button type="submit" class="home-btn btn btn-primary btn-block">
+                                  Join Room
+                              </button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,32 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="/rooms">
-                        @csrf
-                        <div class="form-group row">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Create a public room
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<home-component></home-component>
 @endsection

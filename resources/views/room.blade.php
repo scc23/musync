194ea95aa45d1755f4054742d21386ff309c2bfc
scc23 <sherman_chow@sfu.ca>
2018-10-13 16:23:21 -0700

    Fix syntax and spacing

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -8,19 +8,13 @@
                 <div class="card-header">Room Name: {{ $room_id }}</div>
                 <div class="card-body">
                     <div class="row">
-
-                    
                         <search-component csrf-token="{{csrf_token()}}"></search-component>
                         <playlist-component csrf-token="{{csrf_token()}}"></playlist-component>
                         <user-list-component csrf-token="{{csrf_token()}}"></user-list-component>
-                    
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-    <!-- <search-component csrf-token="{{csrf_token()}}"></search-component>
-    <playlist-component csrf-token="{{csrf_token()}}"></playlist-component>
-    <user-list-component csrf-token="{{csrf_token()}}"></user-list-component> -->
 @endsection

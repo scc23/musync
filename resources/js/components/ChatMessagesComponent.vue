84f4a172c5b415594f97534a66fa844165916294
jdiggidawg <jtran@Jessies-MacBook-Air.local>
2018-11-01 01:35:21 -0700

    Rename and refactor Chat Messages vue component

---
+++ resources/js/components/ChatMessagesComponent.vue
@@ -0,0 +1,53 @@
+<!-- ChatMessages Component -->
+<!-- Accepts an array of messages as props, loops through them and displays the name of the user who sent the message and the message body -->
+
+<template>
+    <div class="chat-messages">
+        <ul class="chat">
+            <li class="left clearfix" v-for="message in dummyMessages">
+                <div class="chat-body clearfix">
+                    <div class="header">
+                        <strong class="primary-font">
+                            {{message.user}}
+                        </strong> 
+                    </div>
+                    <p>
+                        {{message.message}}
+                    </p>
+                </div>
+            </li>
+        </ul> 
+    </div>
+</template>
+
+<script>
+    export default {
+
+        // Dummy messages for display
+        name: 'dummyMessages',
+    
+        data(){
+            return {
+                dummyMessages: [
+                    {
+                        user: 'John',
+                        message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+                    },
+                    {
+                        user: 'Paul',
+                        message: 'Curabitur bibendum ornaredolor, quis ullamcorper ligula sodales.'
+                    },
+                    {
+                        user: 'John',
+                        message: 'Curabitur bibendum ornaredolor, quis ullamcorper ligula sodales.'
+                    },
+                    {
+                        user: 'John',
+                        message: 'Curabitur bibendum ornaredolor, quis ullamcorper ligula sodales.'
+                    },
+                ]
+            }
+        }
+    }
+</script>
+

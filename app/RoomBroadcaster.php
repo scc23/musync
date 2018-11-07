71b3c86e314bf248dbca62b03e4b7c4bfa3108e4
David Quong <dquong@sfu.ca>
2018-11-06 16:25:17 -0800

    Add RoomBroadcaster
    Create RoomBroadcaster model and migration scripts

---
+++ app/RoomBroadcaster.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RoomBroadcaster extends Model
+{
+    protected $fillable = [
+        'room_id',
+        'user_id'
+    ];
+
+    protected $primaryKey = 'room_id';
+
+    public function room()
+    {
+        return $this->belongsTo('App\Room');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+}

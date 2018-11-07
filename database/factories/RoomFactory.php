052c94bfead4ea973a2716cbdd73509e42a1420f
David Quong <dquong@sfu.ca>
2018-11-07 00:16:02 -0800

    Create RoomBroadcasterFactory and RoomBroadcastTest
    Create tests for room broadcasting API, and fix bug relating to
    non-incrementing ID of RoomBroadcast model.

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Room::class, function (Faker $faker) {
     return [
-        'id' => str_random(10),
+        'id' => str_random(4),
         'name' => $faker->name,
         'password' => '',
     ];

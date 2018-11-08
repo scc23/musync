2fae5c3723f461e8fc8b06c30e0001f667ede4d5
David Quong <dquong@sfu.ca>
2018-11-07 18:04:44 -0800

    Separate home and room Vue apps
    Remove app.js and replace with home.js and room.js for the home and room
    web pages respectively.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/home.js', 'public/js')
+   .js('resources/js/room.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/welcome-page.scss', 'public/css');

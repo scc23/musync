9044858b501daa440186e4040056b16f57cb7d17
Jessie Tran <pmtran@sfu.ca>
2018-11-08 16:30:45 -0800

    Fix minor comments.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->command->call('migrate:fresh');
         
         // It is important that the seeders are called in this particular order
-        // as RoomMembership and Message rely on Room and User
+        // as RoomMemberships and Messages rely on Rooms and Users
         $this->call([
             UsersTableSeeder::class,
             RoomsTableSeeder::class,

f0bf87d14aa3f98e98cfc2656eb516945a7a540b
Jessie Tran <pmtran@sfu.ca>
2018-11-08 16:30:17 -0800

    Create a work-around for key pair exists issue in RoomMembershipFactory.

--- database/factories/RoomMembershipFactory.php
+++ database/factories/RoomMembershipFactory.php
@@ -6,18 +6,13 @@ use Faker\Generator as Faker;
 
 $factory->define(App\RoomMembership::class, function (Faker $faker) {
 
+    // It is not possible to generate Room-User pair with existing tables.
+    // Factory doesn't seed 1 by 1 but create some sort of cache before input data into the table all at once,
+    // so we cannot check if the pair exists yet, and might create SQL conflicts.
+    // However, we can choose a random room first and create more users accordingly.
+
     $room_id = App\Room::inRandomOrder()->first()->id;
-    $user_id = App\User::inRandomOrder()->first()->id;
-    
-    // TODO: Conditional check doesn't work
-    $key_exists = function($room_id, $user_id) {
-        return RoomMembership::where([ 'room_id' => $room_id, 'user_id' => $user_id, ])->exists();
-    }; 
-
-    do {
-        $user_id = factory(App\User::class)->create()->id;    
-    } while ( $key_exists($room_id, $user_id) );
-
+    $user_id = factory(App\User::class)->create()->id;
     
     return [
         'room_id' => $room_id,
